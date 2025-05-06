9c506b363fe33cb3a929a11dee8dbb2b0cdcce5b
devcontenidosinteractivos <98177573+AndresDiazPinto@users.noreply.github.com>
2025-05-06 12:44:52 -0500

    Initial commit
    Se realiza portada. intro, sintesis, global

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,9 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    Name: 'Estados financieros de propósito general',
+    Description:
+      'Se desarrollarán las bases para la presentación de los estados financieros de propósito general, bajo las NIIF. Los estados financieros son el resultado de las transacciones realizadas por la empresa en un periodo determinado, los cuales se encuentran integrados por: estado de situación financiera, estado de resultados, estado de cambio en el patrimonio, estado de flujos de efectivo y notas a los estados financieros. ',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
@@ -12,6 +13,10 @@ export default {
       {
         clases: ['banner-principal-decorativo-2'],
         imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.svg'),
       },
     ],
   },
@@ -31,13 +36,48 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Estado de resultados integral',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Identificación',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Periodo contable sobre el que se informa',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Principio del ente en marcha',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo: 'Aspectos generales',
+            hash: 't_1_4',
+          },
+          {
+            numero: '1.5',
+            titulo: 'Cuentas Otro Resultado Integral (ORI)',
+            hash: 't_1_5',
+          },
+          {
+            numero: '1.6',
+            titulo: 'Estructura',
+            hash: 't_1_6',
+          },
+          {
+            numero: '1.7',
+            titulo: 'Forma de presentación',
+            hash: 't_1_7',
+          },
+          {
+            numero: '1.8',
+            titulo: 'Información a revelar ',
+            hash: 't_1_8',
           },
         ],
       },
@@ -45,13 +85,121 @@ export default {
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Estado de situación financiera',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Identificación',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Período contable sobre el que se informa',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Principio del ente en marcha',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo: 'Distinción entre corriente y no corriente',
+            hash: 't_2_4',
+          },
+          {
+            numero: '2.5',
+            titulo: 'Cuentas del Estado de situación financiera',
+            hash: 't_2_5',
+          },
+          {
+            numero: '2.6',
+            titulo: 'Estructura',
+            hash: 't_2_6',
+          },
+          {
+            numero: '2.7',
+            titulo: 'Forma de presentación',
+            hash: 't_2_7',
+          },
+          {
+            numero: '2.8',
+            titulo: 'Información a revelar',
+            hash: 't_2_8',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Estado de cambios en el patrimonio',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Identificación',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Período contable sobre el que se informa',
+            hash: 't_3_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Principio del ente en marcha',
+            hash: 't_3_3',
+          },
+          {
+            numero: '3.4',
+            titulo: 'Estructura',
+            hash: 't_3_4',
+          },
+          {
+            numero: '3.5',
+            titulo: 'Información a revelar',
+            hash: 't_3_5',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Estado de flujos de efectivo ',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '4.1',
+            titulo: 'Identificación',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.2',
+            titulo: 'Período contable sobre el que se informa',
+            hash: 't_4_2',
+          },
+          {
+            numero: '4.3',
+            titulo: 'Elementos para elaborar el flujo de efectivo',
+            hash: 't_4_3',
+          },
+          {
+            numero: '4.4',
+            titulo: 'Métodos de presentación',
+            hash: 't_4_4',
+          },
+          {
+            numero: '4.5',
+            titulo: 'Estructura',
+            hash: 't_4_5',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo: 'Notas y revelaciones',
         desarrolloContenidos: true,
       },
     ],
@@ -102,22 +250,155 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: 'Sitio web',
-      link: '',
+      tema: 'Estados financieros de propósito general.',
+      referencia:
+        'Servicio Nacional de Aprendizaje, SENA. (2018). Revista Finnova: Investigación e Innovación Financiera y Organizacional. ',
+      tipo: 'Capítulo 1 de libro',
+      link: 'http://revistas.sena.edu.co/index.php/finn',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Activo corriente',
+      significado:
+        'una entidad clasificará un activo como corriente cuando espera realizar el activo o tiene la intención de venderlo o consumirlo en su ciclo normal de operación; mantiene el activo principalmente con fines de negociación. ',
+    },
+    {
+      termino: 'Estados financieros',
+      significado:
+        'un juego completo de estados financieros comprende un estado de situación financiera al final del periodo; un estado del resultado integral del periodo; un estado de cambios en el patrimonio del periodo; un estado de flujos de efectivo del periodo.',
+    },
+    {
+      termino: 'Estados financieros con propósito general',
+      significado:
+        'son aquellos que pretenden cubrir las necesidades de usuarios que no están en condiciones de exigir informes a la medida de sus necesidades específicas de información.',
+    },
+    {
+      termino: 'Financial Accounting Standards Board - FASB',
+      significado:
+        'en español traduce Misión del Concejo para los Estándares de Contaduría Financiera y es la entidad encargada de fijar los principios de contabilidad generalmente aceptados en los Estados Unidos. ',
+    },
+    {
+      termino: 'Generally Accepted Accounting Principles – GAAP',
+      significado:
+        'su traducción corresponde a Principios de Contabilidad Generalmente Aceptados y son de uso general en el mundo, los USGAAP son los principios de contabilidad generalmente aceptados en Estados Unidos, mientras que los COLGAAP son los principios de contabilidad generalmente aceptados en Colombia.',
+    },
+    {
+      termino: 'International Accounting Standard - IAS',
+      significado:
+        'Norma Internacional de Contabilidad, fueron todas las normas de Contabilidad emitidas por el Comité de Normas Internacionales de Contabilidad.',
+    },
+    {
+      termino: 'International Accounting Standards Board - IASB',
+      significado:
+        'en español traduce Junta de Normas Internacionales de Contabilidad, fue creada en el año 2001 para ejercer las funciones que desde 1973 y hasta el año 2001 ejerció el IASC. ',
+    },
+    {
+      termino: 'International Accounting Standards Committee - ',
+      significado:
+        'en español es el Comité de Normas Internacionales de Contabilidad, creado en Londres en 1973 con el fin de estudiar y proponer Normas internacionales de Información financiera internacionales y con el apoyo de 9 países (Australia, Canadá, Francia, Alemania, Japón, México, Países Bajos, Reino Unido y Estados Unidos) aunque Estados Unidos fue el único miembro de los países fundadores que decidió no utilizar las normas Emitidas por el IASC en un principio.',
+    },
+    {
+      termino: 'International Accounting Education Standard Board - IAESB',
+      significado:
+        'su sigla en español es IES. Calidad de la educación contable, para poder hacer paralelos en la implementación de las normas contables. ',
+    },
+    {
+      termino: 'International Financial Reporting Standards- IFRS',
+      significado:
+        'en español ha sido traducido como Normas Internacionales de Información Financiera (NIIF) y son las normas que ha emitido desde el 2001 la junta de normas internacionales de Contabilidad.',
+    },
+    {
+      termino: 'NIC',
+      significado:
+        'Norma Internacional de Contabilidad, es la equivalencia de la sigla en inglés IAS (Institute Accounting Standard).',
+    },
+    {
+      termino: 'NIIF',
+      significado:
+        'Norma Internacional de Información Financiera, es la equivalencia de la sigla en inglés IFRS (International Financial Reporting Estandar).',
+    },
+    {
+      termino: 'Pasivo corriente',
+      significado:
+        'una entidad clasificará un pasivo como corriente cuando: espera liquidar el pasivo en su ciclo normal de operación; mantiene el pasivo principalmente con fines de negociación.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Congreso de la República de Colombia. (13 de julio de 2009). Ley 1314 de 2009. ',
+      link:
+        'http://www.secretariasenado.gov.co/senado/basedoc/ley_1314_2009.html',
+    },
+    {
+      referencia:
+        'Consejo Técnico de la Contaduría Pública – CTCP. (2006). Norma Internacional de Contabilidad 7: Estados de flujos de efectivo. ',
+      link:
+        ' https://www.ctcp.gov.co/proyectos/contabilidad-e-informacion-financiera/documentos-organismos-internacionales/compilacion-marcos-tecnicos-de-informacion-financi/1534368973-9784',
+    },
+    {
+      referencia:
+        'Consejo Técnico de la Contaduría Pública – CTCP. (2018). Norma Internacional de Contabilidad 1: Presentación de Estados Financieros. ',
+      link:
+        'https://www.ctcp.gov.co/proyectos/contabilidad-e-informacion-financiera/documentos-organismos-internacionales/compilacion-marcos-tecnicos-de-informacion-financi/1534364977-6992',
+    },
+    {
+      referencia:
+        'Estupiñán, R. (2013). NIC / NIIF transición y adopción en la empresa. Ediciones de la U. ',
       link: '',
+    },
+    {
+      referencia:
+        'Estupiñán, R., y Sierra, A. (2017). Estados financieros básicos: bajo NIC /NIIF. Ecoe Ediciones. ',
+      link: '',
+    },
+    {
+      referencia:
+        'Godoy, E. (2013). Contabilidad y presentación de estados financieros. Grupo Editorial Nueva Legislación.',
+      link: '',
+    },
+    {
+      referencia:
+        'Godoy, E. (2014) Normas Internacionales de información financiera NIIF. Grupo Editorial Nueva Legislación.',
+      link: '',
+    },
+    {
+      referencia:
+        'IFRS Foundation. (2009). Módulo 5: estado del resultado integral y estado de resultados. IFRS Publications. ',
+      link: '',
+    },
+    {
+      referencia: 'NIC NIIF (2009). NIIF para las pymes. ',
+      link: 'https://www.nicniif.org/home/normas/niif-para-las-pymes.html',
+    },
+    {
+      referencia:
+        'Presidencia de la República de Colombia. (23 de diciembre de 2015). Decreto 2420 de 2015 Único Reglamentario de las Normas de Contabilidad, de Información Financiera y de Aseguramiento de la Información',
+      link:
+        'http://www.suin-juriscol.gov.co/viewDocument.asp?ruta=Decretos/30019674',
+    },
+    {
+      referencia:
+        'Presidencia de la República de Colombia. (23 de diciembre de 2015). Decreto 2496 de 2015, por medio del cual se modifica el Decreto 2420 de 2015 Único Reglamentario de las Normas de Contabilidad, de Información Financiera y de Aseguramiento de la Información. ',
+      link:
+        'http://www.suin-juriscol.gov.co/viewDocument.asp?ruta=Decretos/30019674',
+    },
+    {
+      referencia:
+        'Presidencia de la República de Colombia. (27 de marzo de 1971). Decreto-Ley 410 de 1971, por el cual se expide el Código del Comercio. http://www.secretariasenado.gov.co/senado/basedoc/codigo_comercio.html',
+      link: '',
+    },
+    {
+      referencia:
+        'Presidencia de la República de Colombia. (29 de diciembre de 1993). Decreto 2649 de 1993: Reglamento General de Contabilidad. ',
+      link: 'https://niif.com.co/decreto-2649-1993/',
+    },
+    {
+      referencia:
+        'Pymes y autónomos (26 de septiembre de 2013). ¿Cuál es la diferencia entre liquidez y solvencia? ',
+      link:
+        'https://www.pymesyautonomos.com/administracion-finanzas/cual-es-la-diferencia-entre-liquidez-y-solvencia',
     },
   ],
   creditos: [
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -7,6 +7,126 @@
         span
           i.fas.fa-info
       h1 Introducción
+    
+    .bg-full-width.bg-color-2
+      .px-4.px-md-5.pb-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0.order-2
+              figure
+                img(src="@/assets/curso/intro/1.svg", data-aos="zoom-in")
+            .col-lg-8.order-1
+              .bloque-texto-a__texto.p-4
+                p Los estados financieros de propósito general reflejan la situación financiera y el desempeño de la empresa en un periodo determinado, además de los flujos de efectivo; dichos estados son una herramienta administrativa que contribuye a la adecuada toma de decisiones.
+
+        .row.mb-5
+          .col-lg-5.mb-3.mb-lg-0
+            figure
+              img(src="@/assets/curso/intro/2.svg", data-aos="zoom-in")
+          .col-lg-7
+            .bg-color-10.p-5(data-aos="fade-left")
+              p.mb-5 Cabe resaltar que los elementos de los estados financieros son: activo, pasivo, patrimonio, ingresos y gastos. El conjunto de estos se encuentra integrado por:
+              ul.lista-ul--color
+                li.mb-0.d-flex
+                  i.fas.fa-check-circle.bg-color-7
+                  p.mb-0 Estado de situación financiera.
+                li.mb-0.d-flex
+                  i.fas.fa-check-circle.bg-color-7
+                  p.mb-0 Estados de resultados integral.
+                li.mb-0.d-flex
+                  i.fas.fa-check-circle.bg-color-7
+                  p.mb-0 Estado de cambio en el patrimonio.
+                li.mb-0.d-flex
+                  i.fas.fa-check-circle.bg-color-7
+                  p.mb-0 Estado de flujos de efectivo.
+                li.mb-0.d-flex
+                  i.fas.fa-check-circle.bg-color-7
+                  p.mb-0 Notas a los estados financieros.
+        
+        .row.mb-5
+          .col-lg-4.order-2
+            figure
+              img(src="@/assets/curso/intro/3.png", data-aos="zoom-in")
+          .col-lg-8.order-1.mb-3.mb-lg-0
+            p.mb-5(data-aos="fade-left") Los estados financieros se deben presentar por lo menos una vez al año, y en caso de requerirse en períodos intermedios, es importante informar el periodo en el cual se está desarrollando. Al respecto, y de acuerdo con el Consejo Técnico de la Contaduría Pública - CTCP- (2020) en la NIC 1:
+            .bloque-texto-f.color-secundario.pt-4.px-5.mb-5
+              .bloque-texto-f__comillas
+                i.fas.fa-quote-left
+                i.fas.fa-quote-right
+              h4.text-regular.bloque-texto-f__texto Una entidad presentará, como mínimo, dos estados de situación financiera, dos estados del resultado y otro resultado integral del periodo, dos estados del resultado del periodo separados (si los presenta), dos estados de flujos de efectivo y dos estados de cambios en el patrimonio, y notas relacionadas (p. 7). 
+        div.bg-color-4.tarjeta.px-4.py-3.mb-5
+          h4.mb-0 Características      
+        p.mb-5 Para la elaboración y presentación de los estados financieros es importante tener en cuenta las siguientes características.
+
+        .row.mb-5.align-items-center
+          .col-lg-5.mb-3.mb-lg-0
+            figure
+              img(src="@/assets/curso/intro/4.svg", data-aos="zoom-in")
+          .col-lg-7
+            AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta bg-color-11")
+              div(titulo="Presentación razonable")
+                p.mb-0 Es la presentación fidedigna de los estados financieros, teniendo en cuenta los criterios establecidos en las NIIF.
+              div(titulo="Negocio en marcha")
+                p.mb-0 Indica que la empresa debe tener continuidad en el tiempo y, por ende, la contabilidad debe reflejar el flujo de sus operaciones.
+              div(titulo="Base de acumulación")
+                p.mb-0 Hace referencia al reconocimiento de los hechos económicos, los cuales se realizan en el momento que suceden.
+              div(titulo="Importancia relativa")
+                p.mb-0 También denominada materialidad, hace énfasis en el reconocimiento, o no, de un hecho económico. Puede tener un efecto significativo en el proceso contable y en la toma de decisiones.
+              div(titulo="Compensación")
+                p Una entidad no compensará activos con pasivos o ingresos con gastos a menos que así lo requiera o permita una NIIF.
+              div(titulo="Información comparativa")
+                p.mb-0 Se realiza el análisis con respecto al periodo anterior. Debe incluir tanto datos cuantitativos como información cualitativa. 
+              div(titulo="Uniformidad")
+                p Hace referencia a la aplicación de normas, políticas y principios en el reconocimiento de hechos económicos.
+
+    p.mb-5 La Norma Internacional de Contabilidad (NIC) 1 Presentación de Estados Financieros (CTCP, 2020), indica lo siguiente:
+    
+    div.bg-color-4.tarjeta.px-4.py-3.mb-5
+      h4.mb-0 Objetivo
+    
+    .row.mb-5
+      .col-lg-3.order-2
+        figure
+          img(src="@/assets/curso/intro/5.svg", data-aos="zoom-in")
+      .col-lg-9.order-1.mb-3.mb-lg-0
+        .bg-color-10.px-5.py-4
+          p.mb-0(data-aos="fade-left") Esta norma establece las bases de la presentación de los estados financieros de propósito general, para asegurar que los mismos sean comparables, tanto con los estados financieros de la misma entidad correspondientes a periodos anteriores, como con los de otras entidades. Se establecen requerimientos generales con miras a la presentación de los estados financieros, guías para determinar su estructura y requisitos mínimos sobre su contenido. 
+
+    div.bg-color-4.tarjeta.px-4.py-3.mb-5
+      h4.mb-0 Alcance
+
+    .row.mb-5
+      .col-lg-3.order-2
+        figure
+          img(src="@/assets/curso/intro/6.svg", data-aos="zoom-in")
+      .col-lg-9.order-1.mb-3.mb-lg-0
+        p(data-aos="fade-left") Una entidad aplicará esta norma al preparar y presentar estados financieros de propósito de información general conforme a las Normas Internacionales de Información Financiera (NIIF). 
+        .bg-color-10.px-5.py-4
+          p.mb-0(data-aos="fade-left") Es de anotar que en otras NIIF se establecen los requerimientos de reconocimiento, medición e información a revelar para transacciones y otros sucesos.
+    
+    .bg-full-width.bg-color-2
+      .px-4.px-md-5.pb-md-3
+        .row.mb-5
+          .col-lg-4.mb-3.mb-lg-0
+            figure
+              img(src="@/assets/curso/intro/7.svg", data-aos="zoom-in")
+          .col-lg-8
+            p(data-aos="fade-left") Esta norma no es pertinente para la estructura y contenido de los estados financieros intermedios condensados que se elaboren de acuerdo con la NIC 34 Información Financiera Intermedia; sin embargo, los párrafos 15 a 35 se aplicarán a estos estados financieros. Esta NIC 1 se usará de la misma forma en todas las entidades, incluyendo las que presentan estados financieros consolidados de acuerdo con la NIIF 10 Estados Financieros Consolidados y las que presentan estados financieros separados, de acuerdo con la NIC 27 Estados Financieros Separados. 
+            p(data-aos="fade-left") Además, dicha norma utiliza terminología propia de las entidades con ánimo de lucro, incluyendo las pertenecientes al sector público. Si las entidades con actividades sin fines de lucro del sector privado o del sector público aplican esta, podrían verse obligadas a modificar las descripciones utilizadas para partidas específicas de los estados financieros, e incluso para éstos. 
+        .row.mb-5.align-items-center
+          .col-lg-2.mb-3.mb-lg-0.order-lg-2
+            figure
+              img(src="@/assets/curso/intro/8.svg", data-aos="zoom-in")
+          .col-lg-10.order-lg-1
+            p.mb-0(data-aos="fade-left") Similarmente, las entidades que carecen de patrimonio, tal como se define en la NIC 32 Instrumentos Financieros: Presentación (por ejemplo, algunos fondos de inversión colectiva), y las entidades cuyo capital en acciones no es patrimonio (por ejemplo, algunas entidades cooperativas) podrían tener la necesidad de adaptar la presentación en los estados financieros de las participaciones de sus miembros o participantes.
+      
+        .bg-color-10.p-4
+          p.mb-0 <b>Nota aclaratoria</b>: las tablas que se toman como referencia en los ejemplos en cada uno de los temas desarrollados, elaboradas mediante el programa Excel, son derivadas de los mismos, por lo tanto, no se numeran y la elaboración es propia, salvo donde se indique lo contrario.
+
+        
+          
+      
+        
 
 </template>
 
